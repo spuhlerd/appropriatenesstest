ad22f66989d45113761d366ddbb04d76a219bc09
Dorothee Spuhler <dorotheespuhler@dorothees-mbp-2.eawag.wroot.emp-eaw.ch>
2016-01-07 15:05:25 +0100

    ...

--- appropriatness.masterfile.R
+++ appropriatness.masterfile.R
@@ -1,4 +1,13 @@
 rm(list=ls())
+
+## ==============================================================================================
+#TO DOS:
+#- Correct that $tech.app.score is not 1 if tech does not exist... make example...
+#- run even if function not known in casedata...
+#- how to get all the tech.app.scores... e.g. applist_test[[]]$tech.app.score
+#- how to make yes/no or a/b functions
+#- how to make functions more flexibel, e.g. 25, 50, 75%
+#- provide tech options
 ## ==============================================================================================
 setwd("/Users/dorotheespuhler/Dropbox\ (Personal)/PHD\ Dropbox/1\ MODELLING/R/Appropriateness/")
 #command: cd Dropbox/PHD\ Dropbox/1\ MODELLING/R/Appropriateness
@@ -18,15 +27,25 @@ source("compute.applist.r") # function(techlist, caselist, listsep=" ", filename
 source("applist.write.r") # function(applist, listsep=" ", filename="") writes applist
 
 ## ==============================================================================================
-# Create the list of technology appropriateness functions and the list of case appropriateness functions
-#-------------------------------------------
-# Some guidelines to fill in the data files:
+# SOME GUIDELINES TO FILL IN DATA FILES
 #------------------------------------------
 # Each data files contines a list of items (either techs or cases in the columns)
 # Each items has some attributes (lines)
-# The first attributes are just information, that helps to understand the case or is needed for later on
-# Then the appropriateness attributes are listed
-# Each attribute goes over three lines:
+# The first lines (info.row) contain attributes with particular information needed to construct systems or to understand the context.
+# Then the appropriateness attributes are listed.
+#------------------------------------------
+# FOR TECHs
+# Functional groups: U, S, C, T1, T2, D
+# Products: Urine, Faeces, Excreta, Balckwater, Greywater, ...
+# ... Pit humus, Compost, Sludge, Effluent, Stormwater,...
+# ... Treated sludge, Treated effluent, Biogas
+#------------------------------------------
+# FOR CASES
+# Descripton: Just a few lines what the case is about
+# Inhabitants: Number of inhabitants
+#------------------------------------------
+# APPROPRIATENESS ATTRIBUTES
+# Each appropriateness attribute goes over three lines:
 # 1 Name of the attributes to be used: bod, water, temp, omskil, etc.
 # 2 Name of function of function (see below) describing the technology/case requirement/capactiy 
 # 3 Parameters required for this function
@@ -34,15 +53,14 @@ source("applist.write.r") # function(applist, listsep=" ", filename="") writes a
 # req.range(x, lower=-Inf, upper=Inf)
 # req.trapez(x, a, b, c, d),
 # dtriangle(x, a, b, c)
-# dtrapezoid(x, min, mode1, mode2, max )
+# dtrapezoid(x, min, mode1, mode2, max)
 # dunif(x, min, max)
 # dnorm, dlnorm, dbeta, dweibull, dgamma, dlogis, etc.
 
-# read the normal data files
+## ==============================================================================================
+# Create the list of technology appropriateness functions and the list of case appropriateness functions
 caselist<- build.list("casedata.csv",2)
-#str(caselist)
 techlist<- build.list("techdata.csv",3)
-#str(techlist)
 
 ## ==============================================================================================
 # Compute appropriateness profiles (app.profile(tech, case)) and apppropriateness scores (app.scores(tech, case))
@@ -96,11 +114,39 @@ sub.applist[[1]]$tech.app.profile$bod
 applist[[1]]$tech.app.profile$bod # to get the bod of an item in the list, 1 ist the list id
 
 ## ==============================================================================================
-# Use data from Daniel
+# Test data from Daniel
+# Read data
 caselist_daniel<- build.list("casedata_daniel.csv",2)
 techlist_daniel<- build.list("techdata_daniel.csv",3)
-compute.app(caselist_daniel$arbaminch,techlist_daniel$pour.flush, lshowplot=FALSE)
+# Test one by one
+compute.app(caselist_daniel$arbaminch,techlist_daniel$pour.flush, lshowplot=TRUE)
+compute.app(caselist_daniel$arbaminch,techlist_daniel$UDDT, lshowplot=TRUE)
+compute.app(caselist_daniel$arbaminch,techlist_daniel$septic.tank, lshowplot=TRUE)
+compute.app(caselist_daniel$arbaminch,techlist_daniel$single.pit, lshowplot=TRUE)
+compute.app(caselist_daniel$arbaminch,techlist_daniel$wsp, lshowplot=TRUE)
+compute.app(caselist_daniel$arbaminch,techlist_daniel$surface.flow.CW, lshowplot=TRUE)
+# Compute entire list
+applist_daniel<-compute.applist(caselist_daniel,techlist_daniel,lsort=TRUE)
+# Write to screen
+applist.write(applist_daniel)
+# Write to file
+applist.write(applist_daniel, listsep=";", filename="applist_daniel.csv")
 
-caselist_test<- build.list("casedata_test.csv",2)
-techlist_test<- build.list("techdata_test.csv",3)
+## ==============================================================================================
+# Testing data
+# Read data
+caselist_test<- build.list("casedata_ex.csv",2)
+techlist_test<- build.list("techdata_ex.csv",3)
+# Test one by one
+compute.app(caselist_test$arbaminch,techlist_test$uddt, lshowplot=TRUE)
+compute.app(caselist_test$arbaminch,techlist_test$dry.toilet, lshowplot=TRUE)
+compute.app(caselist_test$arbaminch,techlist_test$pour.flush, lshowplot=TRUE)
+compute.app(caselist_test$arbaminch,techlist_test$urine.storagetank, lshowplot=TRUE)
+compute.app(caselist_test$arbaminch,techlist_test$dehydration.vaults, lshowplot=TRUE)
+compute.app(caselist_test$arbaminch,techlist_test$double.vip, lshowplot=TRUE)
+# Compute entire list
 applist_test<-compute.applist(caselist_test,techlist_test,lsort=TRUE)
+# Write to screen
+applist.write(applist_test)
+# Write to file
+applist.write(applist_test, listsep=";", filename="applist_test.csv")
