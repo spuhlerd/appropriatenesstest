05c343d803b29d2bb69274029b6d4d9735abaa30
Xavier Lapillonne <xavier.lapillonne@meteoswiss.ch>
2016-05-05 19:15:31 +0200

    - check that attribute exists for this case

--- compute.techapp.R
+++ compute.techapp.R
@@ -31,35 +31,38 @@ compute.techapp= function(case, tech, lshowplot=FALSE){
   }
   
   for(attr in names(tech$app.fun)){
-    # Calculate app.score
-    f1 = tech$app.fun[[attr]]
-    f2 = case$app.fun[[attr]]
-    attrapp.score = mc.integrate(f1,f2)
-    techapp.profile = c(techapp.profile, attrapp.score)
-    
-    # Plots
-    if(lshowplot){
-      # define plot xlim using max value
-      maxxlim=40000 # max possible value
-      xval=seq(0,maxxlim,1) # vector of values to evaluate the last non-zero point in the intevall 1:maxxlim
-                        # only used for plotting as not precise because uses only integer values
-                        # for functions with max smaller than 1, put 0.1 as intervall in seq
-      techval=tech$app.fun[[attr]](xval) # computing tech$app.fun for xval
-      caseval=case$app.fun[[attr]](xval) # computing case$app.fun for xval
-      Xmaxtech=xval[max(which(techval>0))]   # find the highest xval for which techval is nonzero
-                                             # in case of warning message "In max(which(techval > 0)) : no non-missing arguments to max; returning -Inf", you need to increase maxlim
-      if (Xmaxtech>=maxxlim) Xmaxtech=0.0001 # if tech$app.fun is const until Inf set Xmaxtech to very small in order not to be considered
-      Xmaxcase=xval[max(which(caseval>0))]   # find the highest xval for which caseval is nonzero
-                                             # in case of warning message "In max(which(caseval > 0)) : no non-missing arguments to max; returning -Inf", you need to increase maxlim
-      if (Xmaxcase>=maxxlim) Xmaxcase=0.0001 # if tech$app.fun is const until Inf set Xmaxcase to very small in order not to be considered
-      Xmaxplot=max(Xmaxtech,Xmaxcase)+1 # set the higher of Xmaxcase and Xmactech as Xmax for plots
-      # plot
-      plot(tech$app.fun[[attr]], main=attr, xlab="x", ylab="tech.app.fun", xlim=c(0,Xmaxplot), col=techcolor)
-      cex_label= par("cex")*par("cex.lab")
-      par(new = T)
-      plot(case$app.fun[[attr]], col=casecolor, axes = FALSE, xlab = "x", ylab = "",xlim=c(0,Xmaxplot))
-      axis(side = 4)
-      mtext("case.app.fun", side = 4, line=3,cex = cex_label)
+    #check that this attribute also exist in case$app.fun
+    if (attr %in% names(case$app.fun)){
+      # Calculate app.score
+      f1 = tech$app.fun[[attr]]
+      f2 = case$app.fun[[attr]]
+      attrapp.score = mc.integrate(f1,f2)
+      techapp.profile = c(techapp.profile, attrapp.score)
+      
+      # Plots
+      if(lshowplot){
+        # define plot xlim using max value
+        maxxlim=40000 # max possible value
+        xval=seq(0,maxxlim,1) # vector of values to evaluate the last non-zero point in the intevall 1:maxxlim
+        # only used for plotting as not precise because uses only integer values
+        # for functions with max smaller than 1, put 0.1 as intervall in seq
+        techval=tech$app.fun[[attr]](xval) # computing tech$app.fun for xval
+        caseval=case$app.fun[[attr]](xval) # computing case$app.fun for xval
+        Xmaxtech=xval[max(which(techval>0))]   # find the highest xval for which techval is nonzero
+        # in case of warning message "In max(which(techval > 0)) : no non-missing arguments to max; returning -Inf", you need to increase maxlim
+        if (Xmaxtech>=maxxlim) Xmaxtech=0.0001 # if tech$app.fun is const until Inf set Xmaxtech to very small in order not to be considered
+        Xmaxcase=xval[max(which(caseval>0))]   # find the highest xval for which caseval is nonzero
+        # in case of warning message "In max(which(caseval > 0)) : no non-missing arguments to max; returning -Inf", you need to increase maxlim
+        if (Xmaxcase>=maxxlim) Xmaxcase=0.0001 # if tech$app.fun is const until Inf set Xmaxcase to very small in order not to be considered
+        Xmaxplot=max(Xmaxtech,Xmaxcase)+1 # set the higher of Xmaxcase and Xmactech as Xmax for plots
+        # plot
+        plot(tech$app.fun[[attr]], main=attr, xlab="x", ylab="tech.app.fun", xlim=c(0,Xmaxplot), col=techcolor)
+        cex_label= par("cex")*par("cex.lab")
+        par(new = T)
+        plot(case$app.fun[[attr]], col=casecolor, axes = FALSE, xlab = "x", ylab = "",xlim=c(0,Xmaxplot))
+        axis(side = 4)
+        mtext("case.app.fun", side = 4, line=3,cex = cex_label)
+      }
     }
   }
   
