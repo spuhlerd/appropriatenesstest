b89c1265f2ab8adc6287bd48b18d0dc4219807be
Dorothee Spuhler <dorotheespuhler@Dorothees-MacBook-Pro-2.local>
2016-05-16 19:59:02 +0200

    some minor corrections

--- compute.techapp.R
+++ compute.techapp.R
@@ -41,7 +41,7 @@ compute.techapp= function(case, tech, lshowplot=FALSE){
     techapp.data$tech=techname
     techapp.data$case=casename
     techapp.data$techapp.score=1
-    techapp.data$techapp.profile=as.list(c("attr"="empty"))
+    techapp.data$techapp.profile=as.list(c("attr"="NULL"))
     return(techapp.data)
   }
   
